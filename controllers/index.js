1f9c0a6980d8253d23e372e980c48a71f66f2717
LeoSanGo <souoleo@gmail.com>
2021-04-29 14:51:01 -0300

    Testando query estring e query params

--- controllers/index.js
+++ controllers/index.js
@@ -7,11 +7,27 @@ const pagina1 = (req, res) => {
 };
 
 const calc = (req, res) => {
-  res.send('calculadora');
+  if (req.query.num1 && req.query.num2) {
+    const { num1, num2 } = req.query;
+    const soma = parseFloat(num1) + parseFloat(num2);
+    res.send('A soma é: ' + soma);
+  } else {
+    res.send('calculadora');
+  }
+};
+
+const par = (req, res) => {
+  const isPar = req.params.num % 2 === 0;
+  if (isPar) {
+    res.send('Numero É par');
+  } else {
+    res.send('Numero É impar');
+  }
 };
 
 module.exports = {
   home,
   pagina1,
-  calc
+  calc,
+  par,
 };
--- index.js
+++ index.js
@@ -5,14 +5,7 @@ const port = 3000;
 const indexRouter = require("./routes/index");
 const clientsRouter = require("./routes/clients");
 
-<<<<<<< HEAD
 app.use("/", indexRouter);
 app.use("/clients", clientsRouter);
-=======
-app.use((req,res, next) => {
-    console.log('passou por aqui');
-    res.send('não chama mais nada');
-}); 
->>>>>>> 9833a35356be7b0587c15dd4235b859174626651
 
 app.listen(port, () => console.log("listening on port " + port));
